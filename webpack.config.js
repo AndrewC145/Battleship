0cfa5a855b17ebdee240af3bbf6c6042df4f2070
Andrew Cao <andrewcao618@gmail.com>
2025-01-31 21:02:34 -0500

    added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,10 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+  },
   mode: "development",
 };
